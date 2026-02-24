3bc7dcd549fcb6b53b1203777e0c39b45bb550b5
Arimil <renari@arimil.com>
2026-02-24 03:24:37 -0500

    deploy using github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,18 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-/** @type {import("@sveltejs/vite-plugin-svelte").SvelteConfig} */
-export default {
-  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
-}
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') || process.env.NODE_ENV === 'development' ? '' : process.env.BASE_PATH || ''
+		}
+	}
+};
+
+export default config;
